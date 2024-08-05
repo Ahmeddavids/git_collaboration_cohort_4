d3d3867399ec75534896efb737e6fe746bb4e160
Oluwafolakemi Racheal <folakeatolagbe96@gmail.com>
2024-08-05 14:26:37 +0100

    correction to router

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const{user,login}= require("../Controller/userController.js")
+const{user,login}= require("../controller/userController.js")
 
 //const {authenticate} = require("../middleware/authenticate.js")
 
