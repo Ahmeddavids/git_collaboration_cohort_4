9d57b0b602de2649507204136fcf91641c5dc4c9
ahmed isiaka <ahmeddavids6@gmail.com>
2024-08-05 12:30:52 +0100

    Added cors and json bodyparser

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require('./config/DBConfig');
 const express = require('express');
+const cors = require('cors');
 
 const PORT = process.env.PORT;
 const app = express();
+app.use(cors({origin: "*"}));
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Server is listening to PORT: ${PORT}`);
