848067f79470d00f8573fdd83c1976ace6a7f26e
Lu-Tola07 <tola.praiz@gmail.com>
2024-08-05 12:53:00 +0100

    Creating a User model

---
+++ model/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    userFullName: {
+        type: String
+    },
+    userAddress: {
+        type: String
+    },
+    gender: {
+        type: String,
+        enum: {
+            values: ["Male", "Female"],
+            message: 'Gender can only either Male or Female.'
+        },
+        // required: true
+    },
+    email: {
+        type: String,
+        // unique: true
+    },
+    passWord: {
+        type: String
+    }
+});
+
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
